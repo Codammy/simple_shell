211be20dabd538cbe65a1c8aba575bc7a9375d42
Codammy <damisco005@gmail.com>
2023-03-01 04:16:37 -0800

    0.1

--- main.c
+++ main.c
@@ -1,44 +1,54 @@
 #include "header.h"
 
 /**
- *  * main - simple shell.
- *   * @argc: number of arguments passed at run time.
- *    * @argv: array of pointers to string (passed at run time)
- *     *
- *      * Return: 0.
- *      */
+ * main - simple shell.
+ * @argc: number of arguments passed at run time.
+ * @argv: array of pointers to string (passed at run time)
+ *
+ * Return: 0.
+*/
 int main(int argc __attribute__((unused)), char *argv[])
 {
-		struct stat st;
-			pid_t pid;
-				char *buf, *command;
-					int status;
-						size_t size = 0;
+	struct stat st;
+	pid_t pid;
+	char *buf, *command;
+	int status;
+	size_t size = 0;
 
-							while (1)
-									{
-											pid = fork();
-												if (pid == 0)
-														{
-																	printf("$ ");
-																			if (getline(&buf, &size, stdin) == -1)
-																							exit(1);
-																					command = strtok(buf, "\n");
-																							argv[0] = command;
-																									argv[1] = NULL;
-																											if (stat(command, &st) == -1)
-																														{
-																																		perror("Error:");
-																																					exit(1);
-																																							}
-																													if (execve(argv[0], argv, NULL))
-																																{
-																																				perror("Error:");
-																																							exit(1);
-																																									}
-																														}
-													else
-																wait(&status);
-														}
-								return (0);
+	while (1)
+	{
+                printf("$ ");
+		if (getline(&buf, &size, stdin) == -1)
+		{
+			putchar('\n');
+			exit(0);
+		}
+		if (feof(stdin))
+		{
+			putchar('\n');
+			exit(0);
+		}
+		fflush(NULL);
+
+		pid = fork();
+	if (pid == 0)
+	{
+		command = strtok(buf, "\n");
+		argv[0] = command;
+		argv[1] = NULL;
+		if (stat(command, &st) == -1)
+		{
+			perror("Error:");
+			exit(0);
+		}
+		if (execve(argv[0], argv, NULL))
+		{
+			perror("Error:");
+			exit(0);
+		}
+	}
+	else
+		wait(&status);
+	}
+	return (0);
 }
